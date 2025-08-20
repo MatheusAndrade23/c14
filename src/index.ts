be5e9d612b3fdffc6216be9c58a050b53b02bb20
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2025-08-19 22:12:02 -0300

    chore: express setup

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3333, () => console.log("Server running"));
