5e96059beb1411ff365b0e4210441ad0f139e824
Matheus <matheusandrade.ma2003@gmail.com>
2025-09-01 19:36:13 -0300

    feat: healthcheck

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,13 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+    message: "Server is healthy",
+  });
+});
+
 app.listen(3333, () => console.log("Server running"));
