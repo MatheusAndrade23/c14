b69e5a572785c24e7372b0986c375c7723036ca2
Matheus <matheusandrade.ma2003@gmail.com>
2025-09-27 19:34:33 -0300

    feat: getters

--- src/interfaces/Task.ts
+++ src/interfaces/Task.ts
@@ -1,14 +1,22 @@
 export class Task {
-  protected name: string;
+  protected _name: string;
 
-  protected description: string;
+  protected _description: string;
+
+  get name(): string {
+    return this._name;
+  }
+
+  get description(): string {
+    return this._description;
+  }
 
   constructor(name: string, description: string) {
     if (!name || description) {
       throw new Error("Name and description are required");
     }
 
-    this.name = name;
-    this.description = description;
+    this._name = name;
+    this._description = description;
   }
 }
